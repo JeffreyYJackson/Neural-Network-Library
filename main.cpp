f7ab74e2bf4e73de41cb1b6454355f3b7245d8c6
JeffreyYJackson <79682575+JeffreyYJackson@users.noreply.github.com>
2024-11-06 17:12:10 -0800

    Started the implementation of the initialization of weights

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include<random>
 
 struct NetworkParameters
 {
@@ -15,7 +15,7 @@ class Node{
     public:
         float value;
 
-        void setBias(float biasVal){this->bias = biasVal;}
+        void setBias(float biasVal) {this->bias = biasVal;}
         float getBias(){return this -> bias;}
 
         float calculateValue(std::vector<Node> input, std::vector<float> weights);
@@ -32,6 +32,9 @@ class Network{
         std::vector<std::vector<Node>> layers; //2 dimensional vector storing layers. First layer being input and last layer being output.
         std::vector<std::vector<std::vector<float>>> weights; //3 dimensional vector containing the weights to use in the edges.
 
+        
+
+
     public:
         Network(NetworkParameters param, bool isRandom = true): parameters(param){}
         //Functions to import and save networks.
@@ -44,28 +47,52 @@ class Network{
 
 
         //Functions to retrieve private attributes of the object.
-        std::vector<int> getNodeCount(){return this -> parameters.NodesCount;}
-        int getDepth(){return this -> parameters.depth;}
+        std::vector<int> getNodeCount() {return this -> parameters.NodesCount;}
+        int getDepth() {return this -> parameters.depth;}
 };
 
 //Constructor function.
 Network::Network(NetworkParameters param, bool isRandom = true){
 
-    for(int i = 0; i < param.depth; i++){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    for(int i = 0; i < param.depth; i++) {
         this->buildLayer(this->layers.at(i), param.NodesCount.at(i));
     }
 
+    for(int i = 0; i < param.depth - 1; i++) { //For loop representing the connections between layers.
+
+        //Create a new connection between layers
+        std::vector<std::vector<float>> connection;
+        this->weights.push_back(connection);
+
+        for(int j = 0; j < param.NodesCount.at(i+1); j++){
+            
+            std::vector<float> nextLayerNode;
+            this->weights.at(i).push_back(nextLayerNode);
+
+
+
+
+        }
+
+
+    }
 
 }
 
 //Function to build a layer.
 void Network::buildLayer(std::vector<Node> &nodes, int numberOfNodes){
 
-    for(int i = 0; i < numberOfNodes; i++){
+    for(int i = 0; i < numberOfNodes; i++) {
             Node node = Node();
             nodes.push_back(node);
         }
 }
+
+
+
 
 int main(){
     
